4ef43bbbb55866a7c8d01719f2c1025582e83c97
abellliam <abellliam@gmail.com>
2018-11-20 15:57:47 +1000

    Generated text no longer contains repetition

--- Generate_text.py
+++ Generate_text.py
@@ -2,10 +2,12 @@ from Sequence_generator import gen_seq
 from Sequence_generator import load as load_txt
 from Sequence_generator import save as save_txt
 from Model import create_model
+
 from pickle import load
 from keras.models import load_model
 from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
+
 import random
 from datetime import datetime
 
@@ -13,25 +15,6 @@ from datetime import datetime
 def create_seq_and_model(txt_filename, seq_filename, model_filename, units, epochs):
     gen_seq(txt_filename, seq_filename)
     create_model(seq_filename, model_filename, units, epochs)
-
-
-def gen_text(model, mapping, seed='', use_seed=False, seq_length=10, n_chars=1000, seq_filename=''):
-    in_text = determine_seed(seed, use_seed, seq_filename)
-    for i in range(n_chars):
-        encoded = [mapping[char] for char in in_text]
-        encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
-        encoded = to_categorical(encoded, num_classes=len(mapping))
-        # encoded = encoded.reshape(1, encoded.shape[0], encoded.shape[1])
-        yhat = model.predict_classes(encoded, verbose=0)
-        out_char = ''
-        for char, index in mapping.items():
-            if index == yhat:
-                out_char = char
-                break
-        in_text += out_char
-        if out_char in ['.', '!', '?']:
-            break
-    return in_text
 
 
 def determine_seed(seed, use_seed, seq_filename):
@@ -51,6 +34,44 @@ def determine_seed(seed, use_seed, seq_filename):
         # make sure seed starts with a capital letter
         if sequence[0] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
             return sequence
+
+
+def determine_repetition(line, num_reps):
+    words = line.split()
+    sentencelist = list()
+    for word in words:
+        wordlist = [word]
+        for word_inner in words[words.index(word)+1:]:
+            wordlist.append(word_inner)
+            sentence = ' '.join(wordlist)
+            identical_sentences = [x for x in sentencelist if x == sentence]
+            if len(identical_sentences) == num_reps:
+                return True
+            sentencelist.append(sentence)
+    return False
+
+
+def gen_text(model, mapping, seed='', use_seed=False, seq_length=10, n_chars=1000, seq_filename=''):
+    in_text = determine_seed(seed, use_seed, seq_filename)
+    for j in range(n_chars):
+        encoded = [mapping[char] for char in in_text]
+        encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+        encoded = to_categorical(encoded, num_classes=len(mapping))
+        # encoded = encoded.reshape(1, encoded.shape[0], encoded.shape[1])
+        yhat = model.predict_classes(encoded, verbose=0)
+        out_char = ''
+        for char, index in mapping.items():
+            if index == yhat:
+                out_char = char
+                break
+        in_text += out_char
+        if out_char in ['.', '!', '?']:
+            break
+    if determine_repetition(in_text, num_reps=3):
+        print('REJECTED ' + in_text)
+        return gen_text(model, mapping, seq_length=seq_length, n_chars=n_chars, seq_filename=seq_filename)
+    else:
+        return in_text
 
 
 txt_filename = 'nursery_rhymes_clean.txt'
@@ -75,6 +96,7 @@ for i in range(num_text):
 
 save_txt(out_list, 'output.txt')
 
-for out in out_list:
-    print(out)
+
+
+
 
